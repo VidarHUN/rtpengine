cfd20a8c75d2cb6407dd59071c41fb3314d89fb0
Oded Arbel <oded@geek.co.il>
2020-01-28 12:55:39 +0200

    prevent memory leaks in json_restore_call() and actually do load crypto_params_sdes from JSON

--- daemon/redis.c
+++ daemon/redis.c
@@ -1257,6 +1257,7 @@ static int redis_hash_get_sdes_params(GQueue *out, const struct redis_hash *h, c
 				return 0;
 			return -1;
 		}
+		g_queue_push_tail(out, cps);
 
 		g_queue_push_tail(out, cps);
 		snprintf(key, sizeof(key), "%s-%u", k, iter++);
